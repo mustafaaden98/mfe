9df3581a666c16a9b522af7b34d3caf115781ed2
Mustafa Adenwala <mustafa.adenwala@logmein.com>
2022-01-08 17:10:38 +0530

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
+
+
+export default () => {
+    return(
+        <div>
+            <h1>
+                hi There
+            </h1>
+            <hr />
+            <MarketingApp />
+        </div>
+    )
+}
