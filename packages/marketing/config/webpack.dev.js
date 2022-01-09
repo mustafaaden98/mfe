f68a177023b9ff5e6f7db966b3311693193484e5
Mustafa Adenwala <mustafa.adenwala@logmein.com>
2022-01-09 12:10:37 +0530

    Added container deployment script

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -23,9 +23,7 @@ const devConfig = {
             shared: packageJson.dependencies
         }),
         
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        }), 
+
 
     ]
 };
