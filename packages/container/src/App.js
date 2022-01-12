3ff100da77172ed009c0a14b831a1156644f20ed
Mustafa Adenwala <mustafa.adenwala@logmein.com>
2022-01-12 10:16:08 +0530

    Added header component

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 
 
 export default () => {
     return(
-        <div>
-            <h1>
-                hi There
-            </h1>
-            <hr />
-            <MarketingApp />
-        </div>
+        <BrowserRouter>
+            <div>
+                <Header />
+                <MarketingApp />
+            </div>
+        </BrowserRouter>
     )
 }
